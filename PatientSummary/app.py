2ee88badaf1437c11fc82b17a4844842e39d3075
Durgesh <durgeshcmishra@live.in>
2025-01-15 11:23:04 +0000

    added speech to text api

--- PatientSummary/app.py
+++ PatientSummary/app.py
@@ -5,6 +5,10 @@ from flask_cors import CORS
 from subprocess import run
 import json
 import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
@@ -23,16 +27,14 @@ def upload_audio_file():
     if file.filename == '':
         return jsonify({'message': 'No selected file'}), 400
     if file:
+        if not os.path.exists("uploads"):
+            os.makedirs("uploads")
         filename = secure_filename(file.filename)
         if file.mimetype == 'audio/wav':
             filename += ".wav"
         file.save(os.path.join(os.getcwd(),'uploads', filename))
         # code to get response from server
-        from groq import Groq
-
-
         client = Groq()
-        # client
         filename = "uploads/"+filename
         print("File Name :",filename)
         print("file type :",file.mimetype)
@@ -72,7 +74,67 @@ def upload_audio_file():
 
             return jsonify({'message': transcription.text}), 200
         return jsonify({'message': 'File uploaded successfully'}), 200
-    
+
+@app.post("/speech_to_text")
+def speech_to_text():
+    if 'file' not in request.files:
+        return jsonify({'message': 'No file part'}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'message': 'No selected file'}), 400
+    if file:
+        if not os.path.exists("uploads"):
+            os.makedirs("uploads")
+        filename = secure_filename(file.filename)
+        if file.mimetype == 'audio/wav':
+            filename += ".wav"
+        file.save(os.path.join(os.getcwd(),'uploads', filename))
+        # code to get response from server
+        client = Groq()
+        filename = "uploads/"+filename
+        print("File Name :",filename)
+        print("file type :",file.mimetype)
+        with open(filename, "rb") as f:
+            transcription = client.audio.transcriptions.create(
+            file= (filename,f.read()),
+            model="whisper-large-v3",
+            temperature=0.06,
+            prompt= "You are an helpful assistant of a Doctor. Write response like a play script.",
+            language="en",
+            response_format="verbose_json",
+            )
+            return jsonify({'message': transcription.text}), 200
+        return jsonify({'message': 'File uploaded successfully'}), 200
+
+@app.post("/final_summary")
+def summarize():
+    context = request.get_json()
+    # return context
+    if context:
+                # create summary for patient
+                client = Groq()
+                completion = client.chat.completions.create(
+                    model="llama-3.3-70b-versatile",
+                    messages=[
+                        {
+                            "role": "system",
+                            "content": "You are an helpful assistant. You will be given an script of doctor and patient conversation. Based on the conversation create bullet points for the patient for the disease.Response must be in JSON format."
+                        },
+                        {
+                            "role": "user",
+                            "content": context.get("context")
+                        }
+                    ],
+                    temperature=1,
+                    max_tokens=20240,
+                    top_p=1,
+                    stream=False,
+                    response_format={"type": "json_object"},
+                    stop=None,
+                )
+                return jsonify({"message":context.get("context"),"summary":completion.choices[0].message.model_dump()}), 200
+                # for chunk in completion:
+                #     print(chunk.choices[0].delta.content or "", end="")
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
@@ -108,4 +170,4 @@ def download_file(filename):
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0")
+    app.run(debug=True,host="0.0.0.0",port=8000)
