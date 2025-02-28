03f0714257823932bc989b61dbb62ceefe4eab5b
Durgesh <durgeshcmishra@live.in>
2025-02-28 18:11:51 +0530

    User signup completed

--- PersonalAssitantBot/app/routers/users.py
+++ PersonalAssitantBot/app/routers/users.py
@@ -1,12 +1,20 @@
-from fastapi import APIRouter
-from app.models.users import UserBase
+from fastapi import APIRouter,Body
+from app.models.users import UserBase,create_user,UserCreate
 from app.auth import (
     Annotated,
     Depends,
-    get_current_active_user
+    get_current_active_user,
+    get_password_hash
 )
-
+from app.lib.db import SessionDep
 users_router = APIRouter()
+
+@users_router.post("/users",response_model=UserBase,tags=["users"])
+async def create_new_user(user: Annotated[UserCreate, Body()],
+                          session:SessionDep):
+   user.password = get_password_hash(user.password)
+   user = create_user(session, user)
+   return user
 
 @users_router.get("/users/me/", 
             response_model=UserBase,
@@ -17,11 +25,5 @@ async def read_users_me(
 ):
     return current_user
 
-@users_router.get("/users/", tags=["users"])
-async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
 
-@users_router.get("/users/{username}", tags=["users"])
-async def read_user(username: str):
-    return {"username": username}
 
